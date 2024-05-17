f8dba2a37c3b8706610ee4d12a4fa1ba75a9528a
Raphael Amehe <ramehe@st.ug.edu.gh>
2024-05-17 20:04:36 +0100

    Solution to task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,7 +11,21 @@ function processArray(array) {
     });
 }
 
-module.exports = {
-    processArray
-};
 
+// Task 2
+
+// Function to modify array of strings based on corresponding numbers
+function formatArrayStrings(arraystr, arraynum) {
+    return arraystr.map((str, i) => {
+        if (arraynum[i] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
